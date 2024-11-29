c7df0a9c275cc9673e6386658fe2f77556aded21
Amjm23 <amjm92662@gmail.com>
2024-11-29 16:43:40 +0330

    Delete settings.js

--- settings.js
+++
@@ -1,7 +0,0 @@
-const settings = {
-  ownerName: 'NamaOwnerLu', // Ganti dengan nama owner lu
-  botToken: '7111734303:AAEG_o72rnOChD4m0DWpCOdmsi58KuZXN_A', // Ganti dengan token bot Telegram lu
-  owner: '1908668826', //OWNER user id
-};
-
-module.exports = settings;
